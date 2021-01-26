3f781f7d1b2fcdbd4153817e17976d33e7999876
evysotsky <70786069+evysotsky@users.noreply.github.com>
2021-01-26 19:23:54 +0700

    Added Sinus

---
+++ Lab2/Sin.cpp
@@ -0,0 +1,21 @@
+#include "cmath"
+#include "Operation.h"
+#include "OperationMaker.h"
+#include "Exceptions.h"
+
+#define PI 3.14159265
+
+class Sin : public Operation {
+public :
+    void run(std::list<std::string>& args, Context& context) override {
+        if (context.stackIsEmpty()) {
+            throw badArgsException("No operand for 'Sin'");
+        }
+        if (!args.empty()) {
+            throw badArgsException("Too much arguments for 'Sin'");
+        }
+        context.push(sin(context.pop() * PI / 180));
+    }
+};
+
+ADD_OPERATION(Sin, SIN)
